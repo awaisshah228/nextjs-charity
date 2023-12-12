a63c864844f5c5b70298f22f0cb615ea046cee54
Muhammad Awais Shah <m.awaisshah228@gmail.com>
2023-12-12 23:02:23 +0500

    solved result page

--- components/PrintObject.tsx
+++ components/PrintObject.tsx
@@ -6,5 +6,5 @@ export default function PrintObject({
   content: Stripe.PaymentIntent | Stripe.Checkout.Session
 }): JSX.Element {
   const formattedContent: string = JSON.stringify(content, null, 2)
-  return <p>{formattedContent}</p>
+  return (<pre>{formattedContent}</pre>)
 }
